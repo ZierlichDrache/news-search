2f5074b79eee25fb9349f0041eef11e112dfe0b8
ZierlichDrache <zierlich.drache@gmail.com>
2019-09-28 00:58:46 +0200

    Added dtos for the web client

---
+++ news-search-web/src/app/shared/dtos.ts
@@ -0,0 +1,16 @@
+
+export interface Article {
+    author: string,
+    title: string,
+    description: string,
+    date: string,
+    sourceName: string,
+    articleUrl: string,
+    imageUrl: string
+}
+
+export interface News {
+    country: string,
+    category: string,
+    articles: Article[]
+}
