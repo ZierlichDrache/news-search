930eaccd2ddd04892b955e6966d0eda0a303d54d
ZierlichDrache <zierlich.drache@gmail.com>
2019-09-29 16:57:25 +0200

    Added swagger for document our rest api

--- news-search-app/src/main/java/com/consdata/solejnik/newssearch/rest/NewsController.java
+++ news-search-app/src/main/java/com/consdata/solejnik/newssearch/rest/NewsController.java
@@ -3,6 +3,7 @@ package com.consdata.solejnik.newssearch.rest;
 import com.consdata.solejnik.newssearch.dto.News;
 import com.consdata.solejnik.newssearch.service.NewsSearchService;
 import com.consdata.solejnik.newssearch.util.QueryParamsBuilder;
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(description = "Set of endpoints operations of articles.")
 public class NewsController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NewsController.class);
@@ -22,12 +24,28 @@ public class NewsController {
     }
 
     @GetMapping("/news/{country}/{category}")
-    @CrossOrigin
-    public News searchArticles(@PathVariable final String country,
+    @CrossOrigin(origins = "http://localhost:4200")
+    @ApiOperation(value = "Search for articles based of specified params.", code = 200)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Found results", response = News.class),
+            @ApiResponse(code = 400, message = "Invalid newsapi query"),
+            @ApiResponse(code = 400, message = "Invalid page params"),
+            @ApiResponse(code = 401, message = "The newsapi key is invalid"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 429, message = "Too many requests from newsapi"),
+            @ApiResponse(code = 500, message = "Parsing newsapi error message error"),
+            @ApiResponse(code = 503, message = "Newsapi is unavailable")})
+    public News searchArticles(@ApiParam("The language and origin country of an article as two letters. Cannot be empty.")
+                               @PathVariable final String country,
+                               @ApiParam("The category of an article. Cannot be empty.")
                                @PathVariable final String category,
+                               @ApiParam("Text which contains an article. Can be empty.")
                                @RequestParam(value = "q", required = false) final String query,
+                               @ApiParam("The page size for paginated search. Can be empty.")
                                @RequestParam(value = "pageSize", required = false) final Long pageSize,
-                               @RequestParam(value = "page", required = false) final Long page) {
+                               @ApiParam("The page number for paginated search. Can be empty.")
+                               @RequestParam(value = "page", required = false) final Long page
+    ) {
         LOGGER.info("Initializing searching for the articles");
 
         final QueryParamsBuilder paramsBuilder = new QueryParamsBuilder()
